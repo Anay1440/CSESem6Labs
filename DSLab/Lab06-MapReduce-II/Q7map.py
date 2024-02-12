6a9a50790821ef382583033a28f87aa3cce29178
Anay1440 <anay.patil@gmail.com>
2024-02-12 16:04:32 +0530

    DSLab06 complete

---
+++ DSLab/Lab06-MapReduce-II/Q7map.py
@@ -0,0 +1,16 @@
+import random
+
+with open('random_numbers.txt', 'w') as file:
+    for i in range(500):
+        n = random.randint(1, 10000)
+        file.write(str(n) + "\n")
+
+with open('random_numbers.txt', 'r') as file:
+    for line in file:
+        num = line.strip()[:-1] # to remove the \n
+        if (num == ''):
+            continue
+        if (int(num) % 2 == 0):
+            print(f"Even\t{1}")
+        else:
+            print(f"Odd\t{1}")
